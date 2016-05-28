b64680f86ba261217d037c3b1da0f00f13f6fd2f
mlipinski2 <michal.lipinski@virtlab.pl>
2016-05-28 11:31:40 +0200

    Update countdown.js
    1

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -7,9 +7,9 @@
 */
 /* Start Countdown Settings */
 
-var startDate = new Date("16/03/2016"); //THIS IS JUST FOR REFERENCE- DO NOT CHANGE THIS.
+var startDate = new Date("16/03/2014"); //THIS IS JUST FOR REFERENCE- DO NOT CHANGE THIS.
 
-var endDate = new Date("29/05/2016"); //CHANGE THIS TO YOUR LAUNCHING DATE
+var endDate = new Date("30/05/2016"); //CHANGE THIS TO YOUR LAUNCHING DATE
 
 var dif = endDate.getTime() - startDate.getTime();
 var difToSecond = dif / 1000;
